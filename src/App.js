71fafa6d2806d8c7789f05f2d1f1a5da2fb40374
Alex Sanchez <alejandrayemanyasanchez@gmail.com>
2026-02-04 16:27:55 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import profile from './fotocv.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={profile} className="App-logo" alt="logo" />
+        <h1>
+          Evaluacion Parcial 1
+        </h1>
+        <h1>
+           Alumno(a): Sanchez Terrones Alejandra Yemanya
+        </h1>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/alejandra-yemanya?utm_source=share_via&utm_content=profile&utm_medium=member_ios"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+                  LINK DE MI PERFIL
+        </a>
+        <br></br>
+         <a
+          className="App-link"
+          href="https://www.medikt.com.mx/practicas/documentacion.html"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+                  DOCUMENTACION PARCIAL 1
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Prueba_componente.js
@@ -0,0 +1,10 @@
+import './App.css';
+function Prueba_componente(){
+return(
+    <div className="App">
+        ESTA ES MI PRUEBA DE NUEVO COMPONENTE
+    </div>
+
+);
+}
+export default Prueba_componente;
